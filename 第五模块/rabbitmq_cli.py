7032f4faae4593b82be67579da97c30e1914d354
w312752508 <312752508@qq.com>
2018-03-16 20:03:38 +0800

    ss

--- 第五模块/rabbitmq_cli.py
+++ 第五模块/rabbitmq_cli.py
@@ -1,12 +1,14 @@
 import pika
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    'localhost'))
 channel = connection.channel()
-channel.queue_declare(queue="test")
+channel.queue_declare(queue="test2")
 
-def callback(ch,met,pro,body):
+def callback(ch,method,pro,body):
     print("%r"%body)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
-connection.basic_consume(callback,
-                         queue="test",
+channel.basic_consume(callback,
+                         queue="test2",
                          no_ack=True)
 channel.start_consuming()
--- 第五模块/rabbitmq_ser.py
+++ 第五模块/rabbitmq_ser.py
@@ -1,10 +1,10 @@
 # import pika
 # connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 # channel = connection.channel()
-# channel.queue_declare(queue="test")
+# channel.queue_declare(queue="test2")
 # channel.basic_publish(exchange="",
-#                       routing_key="test",
-#                       body="frist test message")
+#                       routing_key="test2",
+#                       body="frist test2 message")
 # print("start send message...")
 # connection.close()
 
@@ -15,11 +15,11 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
 channel = connection.channel()
 
 # 声明queue
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='test2')
 
 # n RabbitMQ a message can never be sent directly to the queue, it always needs to go through an exchange.
 channel.basic_publish(exchange='',
-                      routing_key='hello',
+                      routing_key='test2',
                       body='Hello World!')
 print(" [x] Sent 'Hello World!'")
 connection.close()
